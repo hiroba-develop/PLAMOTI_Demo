016b93e1c37a9e4f47fbf4fe991cc256444c0f8f
yuta-toshino1931 <yuta-toshino@hiroba1931.co.jp>
2025-07-29 12:39:31 +0900

    雛形作成

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// @ts-ignore
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/PLAMOTI_Demo",
+  publicDir: "public",
+  server: {
+    port: 5173,
+    allowedHosts: ["localhost:5173", "35.74.40.37:5173", "product.jp"],
+  },
+});
